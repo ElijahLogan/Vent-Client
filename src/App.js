404337057a147de3efc39ea4b1f4ab92f775ed4d
eli <elijahizelllogan@gmail.com>
2020-05-17 18:25:08 +0000

    add the NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+
+
+import React from "react";
 import Routes from "./Routes";
-import { Navbar } from "react-bootstrap";
 import "./App.css";
+
+
 
 function App() {
   return (
@@ -10,14 +14,21 @@ function App() {
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">Vent</Link>
+            <Link to="/">Scratch</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem href="/signup">Signup</NavItem>
+            <NavItem href="/login">Login</NavItem>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
   );
 }
+
 
 export default App;
