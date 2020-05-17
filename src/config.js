7d8205f6d4142edef608fba38f5b600c4bc39e7f
eli <elijahizelllogan@gmail.com>
2020-05-17 21:10:46 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://gyn5rwcr82.execute-api.us-east-1.amazonaws.com/prodL"
+  },
+  cognito: {
+    REGION: "us-east-2 ",
+    USER_POOL_ID: "us-east-2_JIenezULO",
+    APP_CLIENT_ID: "8hdp6128tte7uksdc0at1h3jsD",
+    IDENTITY_POOL_ID: "us-east-2:e5761ce2-d7df-4d17-bbae-2d37329e541b"
+  }
+};
