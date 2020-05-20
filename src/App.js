e8b9802b459e3f6efb8aafd9e510350daea163a2
eli <elijahizelllogan@gmail.com>
2020-05-20 22:23:12 +0000

    implemented login and delete logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Nav, Navbar, NavItem} from "react-bootstrap";
+import { AppContext } from "./libs/contextLib";
+import {  LinkContainer } from "react-router-bootstrap";
 
 
-import React from "react";
+import React, { useState } from "react";
 import Routes from "./Routes";
 import "./App.css";
 
 
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false)
+
+
+function handleLogout(){
+  userHasAuthenticated(false)
+}
+
   return (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
@@ -19,16 +28,30 @@ function App() {
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-          <Nav pullRight>
-            <NavItem href="/signup">Signup</NavItem>
-            <NavItem href="/login">Login</NavItem>
+  <Nav pullRight>
+            {isAuthenticated
+  ? <NavItem onClick={handleLogout}>Logout</NavItem>
+  : <>
+      <LinkContainer to="/signup">
+        <NavItem>Signup</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/login">
+        <NavItem>Login</NavItem>
+      </LinkContainer>
+    </>
+}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+   <AppContext.Provider value = {{ isAuthenticated, userHasAuthenticated }}>
+      <Routes/>
+   </AppContext.Provider>
     </div>
   );
 }
 
 
 export default App;
+
+
+
