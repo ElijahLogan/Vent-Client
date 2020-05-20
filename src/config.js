7a904ba9da892003f45447a6bf7086c8f9e041d5
eli <elijahizelllogan@gmail.com>
2020-05-20 22:24:24 +0000

    fixed userpool client id error

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default {
   cognito: {
     REGION: "us-east-2 ",
     USER_POOL_ID: "us-east-2_JIenezULO",
-    APP_CLIENT_ID: "8hdp6128tte7uksdc0at1h3jsD",
+    APP_CLIENT_ID: "8hdp6128tte7uksdc0at1h3js",
     IDENTITY_POOL_ID: "us-east-2:e5761ce2-d7df-4d17-bbae-2d37329e541b"
   }
 };
