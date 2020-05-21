1046c79212a8ad9a8f6d3c1185f37ba6e76e488d
eli <elijahizelllogan@gmail.com>
2020-05-21 12:08:02 +0000

    created working signup page with confirmation code

---
+++ src/containers/Signup.js
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import {
+  HelpBlock,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from "react-bootstrap";
+import { useAppContext } from "../libs/contextLib";
+import { useFormFields } from "../libs/hooksLib";
+import { Auth } from "aws-amplify";
+
+import "./Signup.css";
+
+export default function Signup() {
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    confirmationCode: "",
+  });
+  const history = useHistory();
+  const [newUser, setNewUser] = useState(null);
+  const { userHasAuthenticated } = useAppContext();
+
+  function validateForm() {
+    return (
+      fields.email.length > 0 &&
+      fields.password.length > 0 &&
+      fields.password === fields.confirmPassword
+    );
+  }
+
+  function validateConfirmationForm() {
+    return fields.confirmationCode.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+
+   try {
+    const newUser = await Auth.signUp({
+      username: fields.email,
+      password: fields.password,
+    });
+   
+    setNewUser(newUser);
+  } catch (e) {
+      console.log(e)
+      }
+
+  }
+
+  async function handleConfirmationSubmit(event) {
+    event.preventDefault();
+
+  try {
+    await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+    await Auth.signIn(fields.email, fields.password);
+
+    userHasAuthenticated(true);
+    history.push("/");
+  } catch (e) {
+      console.log(e)
+  }
+  }
+
+  function renderConfirmationForm() {
+    return (
+      <form onSubmit={handleConfirmationSubmit}>
+        <FormGroup controlId="confirmationCode" bsSize="large">
+          <ControlLabel>Confirmation Code</ControlLabel>
+          <FormControl
+            autoFocus
+            type="tel"
+            onChange={handleFieldChange}
+            value={fields.confirmationCode}
+          />
+          <HelpBlock>Please check your email for the code.</HelpBlock>
+        </FormGroup>
+        <button
+          type="submit"
+       
+onClick={!validateConfirmationForm()}
+        >
+          Verify
+        </button>
+      </form>
+    );
+  }
+
+  function renderForm() {
+    return (
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            type="password"
+            value={fields.password}
+            onChange={handleFieldChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="confirmPassword" bsSize="large">
+          <ControlLabel>Confirm Password</ControlLabel>
+          <FormControl
+            type="password"
+            onChange={handleFieldChange}
+            value={fields.confirmPassword}
+          />
+        </FormGroup>
+        <button
+          type = "submit"
+          type="submit"
+         onClick={!validateForm()}
+        >
+          Signup
+        </button>
+      </form>
+    );
+  }
+
+  return (
+    <div className="Signup">
+      {newUser === null ? renderForm() : renderConfirmationForm()}
+    </div>
+  );
+}
