b7cee0a4ec3b4dbc892d0bc4f6514648b0f2a39d
eli <elijahizelllogan@gmail.com>
2020-05-27 11:23:20 +0000

    added route to list endpoint

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { useAppContext } from "../libs/contextLib";
-
+import { LinkContainer } from "react-router-bootstrap";
+import { API }  from "aws-amplify";
 import "./Home.css";
+
 
 
 export default function Home() {
@@ -10,15 +12,62 @@ export default function Home() {
   const { isAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
 
-  function renderNotesList(notes) {
-    return null;
+
+useEffect(
+  () => {
+  async function onLoad() {
+    console.log(notes)
+    if (!isAuthenticated) {
+      return;
+    }
+
+    try {
+      const notes = await loadNotes();
+      setNotes(notes);
+      console.log(notes)
+    } catch (e) {
+      console.log(e);
+    }
+
+
   }
+
+  onLoad();
+},[isAuthenticated]);
+
+function loadNotes() {
+  return API.get("notes", "/list");
+}
+
+function renderNotesList(notes) {
+  return [{}].concat(notes).map((note, i) =>
+    i !== 0 ? (
+      <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+        <ListGroupItem header={note.content.trim().split("\n")[0]}>
+          {"Created: " + new Date(note.createdAt).toLocaleString()}
+        </ListGroupItem>
+      </LinkContainer>
+    ) : (
+      <LinkContainer key="new" to="/notes/new">
+        <ListGroupItem>
+          <h4>
+            <b>{"\uFF0B"}</b> Create a new note
+          </h4>
+        </ListGroupItem>
+      </LinkContainer>
+    )
+  );
+}
+
+
 
   function renderLander() {
     return (
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        
+        <p> thank you for staying with us through all the madness </p>
       </div>
     );
   }
@@ -28,15 +77,16 @@ export default function Home() {
       <div className="notes">
         <PageHeader>Your Notes</PageHeader>
         <ListGroup>
-          {renderNotesList(notes)}
+
+{ renderNotesList(notes)}
         </ListGroup>
       </div>
     );
   }
 
   return (
-    <div className="Home">
-      {isAuthenticated ? renderNotes() : renderLander()}
+<div className="Home">
+      { isAuthenticated ? renderNotes()  : renderLander() }
     </div>
   );
 }
